722716885bab955946f8dbf7e3a9f5b8a70eff40
Ndjstice <spelerjamer3@gmail.com>
2024-10-17 14:09:35 -0300

    v0.1

--- main.cpp
+++ main.cpp
@@ -17,9 +17,19 @@ int main() {
     mostrarMenu();
     Equipo club;
     archivoEquipo reg("Equipos.dat");
+<<<<<<< Updated upstream
     club.cargar();
     //reg.agregarRegistro(club);
     //reg.eliminarRegistro("River Plate");
     reg.modificarRegistro(club); ///ARREGLAR!!!!!!!
+=======
+    //club = reg.listarRegistro(4);
+    //club.mostrarEquipo();
+    //club.cargar();
+    //reg.agregarRegistro(club);
+    //reg.eliminarRegistro("River Plate");
+    //reg.modificarRegistro(club); ///ARREGLAR!!!!!!!
+    //reg.buscarRegistro();
+>>>>>>> Stashed changes
     reg.mostrarRegistro();
 }
